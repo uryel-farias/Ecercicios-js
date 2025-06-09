0ee37dd41de964967981f5bc801f0c24e5884e6c
Uryel Farias <uryel.farias@gmail.com>
2025-06-09 11:06:43 -0300

    Finalizando exercicio 4, login

--- Exercicios/01-Condicionais/login.js
+++ Exercicios/01-Condicionais/login.js
@@ -6,3 +6,9 @@ const senha = 321;
 
 let user = readlineSync.question('Digite o seu usuario: ');
 let pass = parseInt(readlineSync.question('Digite sua senha númerica: '));
+
+if (user === usuario && pass === senha) {
+    console.log("Usuário correto.")
+} else {
+    console.log("uuario ou senha incorreto.")
+}
