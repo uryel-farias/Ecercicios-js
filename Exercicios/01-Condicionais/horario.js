b0c894ae448560b0c597510c6782084a099eda50
Uryel Farias <uryel.farias@gmail.com>
2025-06-10 10:48:55 -0300

    Exercicio 5, horario convertido

---
+++ Exercicios/01-Condicionais/horario.js
@@ -0,0 +1,16 @@
+var readlineSync = require('readline-sync');
+
+let hora = readlineSync.question('Digite o horário: ');
+let horaConvertida = hora - 12;
+
+if(hora === 0) {
+    console.log(`A hora e 12 hrs da noite.`)
+} else if (hora > 0 && hora <= 12) {
+    console.log(`A hora é ${hora}00 hrs da manha.`)
+} else if (hora > 12 && hora <= 23) {
+    console.log(`A hora é ${horaConvertida}00 hrs da noite.`)
+}else if (hora > 23) {
+    console.log("Digite um horario valido.")
+} else {
+    console.log("Digite um horario valido.")
+}
