7be5cda07ba85c0fba3bf58bb58ac49a2d1f7e22
Uryel Farias <uryel.farias@gmail.com>
2025-06-11 20:42:34 -0300

    Exercicio 6, media das notas, mostrando a media.

--- Exercicios/01-Condicionais/notas.js
+++ Exercicios/01-Condicionais/notas.js
@@ -7,9 +7,12 @@ let nota3 = parseFloat(readlineSync.question('Digite a terceira nota: '));
 let media = (nota1 + nota2 + nota3) / 3 ;
 
 if (media >= 7) {
+    console.log(`Media do aluno: ${media.toFixed(1)}`)
     console.log("Aprovado")
-} else if () {
-
+} else if (media >= 5 && media < 7) {
+    console.log(`Media do aluno: ${media.toFixed(1)}`)
+    console.log("Recuperação")
 } else {
-
+    console.log(`Media do aluno: ${media.toFixed(1)}`)
+    console.log("Reprovado")
 }
