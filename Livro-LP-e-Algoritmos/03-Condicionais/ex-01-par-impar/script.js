5d3c7283d7bdea224ba235c4c1619b58b36c73f9
Uryel Farias <uryel.farias@gmail.com>
2025-06-12 11:11:50 -0300

    Exercicio 1 do cap 2 do livro

--- Livro-LP-e-Algoritmos/03-Condicionais/ex-01-par-impar/script.js
+++ Livro-LP-e-Algoritmos/03-Condicionais/ex-01-par-impar/script.js
@@ -1,15 +1,17 @@
-const resposta1 = document.querySelector('#medicamento');
-const resposta2 = document.querySelector('#valor-desconto');
+const resultado = document.querySelector('#resultado')
 const botao = document.querySelector('#botao');
 
 botao.addEventListener("click", () => {
 
-    const medicamento = document.querySelector('#descricao-produto').value;
-    const preco = Number(document.querySelector('#valor-produto').value);
-
-    resposta1.innerText = (`Promoção de ${medicamento}.`);
-    resposta2.innerText = (`Leve 2 por apenas R$: ${Math.floor(preco * 2)},00.`);
-
+    let numero = Number(document.querySelector('#numero').value);
+    
+    if ( numero === 0 ) {
+        resultado.innerText = (`${numero} é par.`);
+    } else if ( numero % 2 === 0 ) {
+        resultado.innerText = (`${numero} é par.`);
+    } else {
+        resultado.innerText = (`${numero} é ímpar.`);
+    }
+   
     console.log(resposta1.innerText);
-    console.log(resposta2.innerText);
 })
