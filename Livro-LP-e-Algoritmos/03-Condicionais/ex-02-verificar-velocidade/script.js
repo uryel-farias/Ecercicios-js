ef97203d117f9e78ab67334d76e37fc5b83ff3c8
Uryel Farias <uryel.farias@gmail.com>
2025-06-17 11:01:09 -0300

    Arquivos base para o segundo exercicio

---
+++ Livro-LP-e-Algoritmos/03-Condicionais/ex-02-verificar-velocidade/script.js
@@ -0,0 +1,13 @@
+const resultado = document.querySelector('#resultado')
+const botao = document.querySelector('#botao');
+
+function LimpaCampo() {
+    let campo = document.querySelector('#numero').value='';
+    return campo;
+}
+
+botao.addEventListener("click", () => {
+
+
+
+})
