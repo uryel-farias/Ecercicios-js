7639f4df66aac85df8daf696408fa4d956dd342c
Uryel Farias <uryel.farias@gmail.com>
2025-06-17 08:56:37 -0300

    limmpando o compo do input

--- Livro-LP-e-Algoritmos/03-Condicionais/ex-01-par-impar/script.js
+++ Livro-LP-e-Algoritmos/03-Condicionais/ex-01-par-impar/script.js
@@ -1,17 +1,24 @@
 const resultado = document.querySelector('#resultado')
 const botao = document.querySelector('#botao');
 
+function LimpaCampo() {
+    let campo = document.querySelector('#numero').value='';
+    return campo;
+}
+
 botao.addEventListener("click", () => {
 
     let numero = Number(document.querySelector('#numero').value);
     
     if ( numero === 0 ) {
         resultado.innerText = (`${numero} é par.`);
+        LimpaCampo();
     } else if ( numero % 2 === 0 ) {
         resultado.innerText = (`${numero} é par.`);
+        LimpaCampo();
     } else {
         resultado.innerText = (`${numero} é ímpar.`);
+        LimpaCampo();
     }
-   
-    console.log(resposta1.innerText);
+
 })
