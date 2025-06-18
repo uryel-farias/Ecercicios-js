15b20d2df663cafecb5a422f2417bba235aa3c44
Uryel Farias <uryel.farias@gmail.com>
2025-06-18 16:11:42 -0300

    inicio da logica do exercicio

--- Livro-LP-e-Algoritmos/03-Condicionais/ex-02-verificar-velocidade/script.js
+++ Livro-LP-e-Algoritmos/03-Condicionais/ex-02-verificar-velocidade/script.js
@@ -2,12 +2,24 @@ const resultado = document.querySelector('#resultado')
 const botao = document.querySelector('#botao');
 
 function LimpaCampo() {
-    let campo = document.querySelector('#numero').value='';
+    let campo = document.querySelector('#velocidade_permitida').value='';
     return campo;
 }
 
 botao.addEventListener("click", () => {
 
+    const velocidadePermitida = Number(document.querySelector('#velocidade_permitida').value);
+    const velocidadeCondutor = Number(document.querySelector('#velocidade_condutor').value);
+
+    if (velocidadeCondutor <= velocidadePermitida) {
+        resultado.innerText = 'Sem multa';
+    } else if (velocidadeCondutor > velocidadePermitida &&
+        velocidadeCondutor <= velocidadePermitida * 1.2
+     ) {
+        resultado.innerText = 'Multa Leve';
+    } else {
+        resultado.innerText = 'Multa Grave';
+    }
 
 
 })
