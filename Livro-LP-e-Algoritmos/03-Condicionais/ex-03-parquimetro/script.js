b7175122ae8c0d8801d13a0b738600f5015cff97
Uryel Farias <uryel.farias@gmail.com>
2025-06-30 09:19:14 -0300

    inicio do exercicio 3, parquimetro

--- Livro-LP-e-Algoritmos/03-Condicionais/ex-03-parquimetro/script.js
+++ Livro-LP-e-Algoritmos/03-Condicionais/ex-03-parquimetro/script.js
@@ -1,8 +1,18 @@
-const resultado = document.querySelector('#resultado');
+const resultado1 = document.querySelector('#resultado1');
+const resultado2 = document.querySelector('#resultado2');
 const botao = document.querySelector('#botao');
 
 
 botao.addEventListener("click", () => {
+    const valor = Number(document.querySelector('#valor').value);
 
+    if (valor < 1 ) {
+        resultado1.innerText = 'Valor insuficiente.'
+    } else if (valor >= 1 && valor <= 1.75) {
+        resultado1.innerText = 'Tempo: 30 min.'
+        resultado2.innerText = `Troco R$: ${(valor - 1).toFixed(2)}`
+   }
+
+    console.log(valor);
 
 })
