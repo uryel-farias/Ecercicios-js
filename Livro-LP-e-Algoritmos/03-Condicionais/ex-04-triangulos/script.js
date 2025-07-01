ab07054b348bddf71ecdc08c0260e163e1639a2f
Uryel Farias <uryel.farias@gmail.com>
2025-06-30 23:29:14 -0300

    inicio da logica do EXERCICIO 4

--- Livro-LP-e-Algoritmos/03-Condicionais/ex-04-triangulos/script.js
+++ Livro-LP-e-Algoritmos/03-Condicionais/ex-04-triangulos/script.js
@@ -2,3 +2,33 @@ const resultado1 = document.querySelector('#resultado1');
 const resultado2 = document.querySelector('#resultado2');
 const botao = document.querySelector('#botao');
 
+let triangulo = true;
+
+botao.addEventListener('click', ()=>{
+
+    const ladoA = Number(document.querySelector('#ladoA').value);
+    const ladoB = Number(document.querySelector('#ladoB').value);
+    const ladoC = Number(document.querySelector('#ladoC').value);
+
+    console.log(ladoA,ladoB,ladoC)
+
+    if (ladoA > (ladoB + ladoC)) {
+        triangulo = false;
+    } else if (ladoB > (ladoA + ladoC)) {
+        triangulo = false;   
+    } else if ( ladoC > (ladoA + ladoB)) {
+        triangulo = false;  
+    } else {
+        triangulo = true;
+    }
+
+        console.log(triangulo)
+
+    if (triangulo) {
+        const equilatero = trianguloEquilatero( ladoA, ladoB, ladoC ); 
+        const isosceles = trianguloIsosceles( ladoA, ladoB, ladoC ); 
+        const escaleno = trianguloEscaleno( ladoA, ladoB, ladoC ); 
+
+    }
+
+})
