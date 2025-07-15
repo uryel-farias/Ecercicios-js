8468370123ab03805c053ab70de051c7065d2985
Uryel Farias <uryel.farias@gmail.com>
2025-07-14 23:55:24 -0300

    iniciando exercicio 1 do capitulo de repetições

---
+++ Livro-LP-e-Algoritmos/04-Lacos/ex-01-repete-fruta/script.js
@@ -0,0 +1,9 @@
+const botao  = document.querySelector('#botao');
+
+botao.addEventListener("click", () => {
+    const fruta = document.querySelector('#fruta').value;
+    const quantidade = document.querySelector('#numero').value;
+})
+
+console.log(fruta);
+console.log(quantidade);
