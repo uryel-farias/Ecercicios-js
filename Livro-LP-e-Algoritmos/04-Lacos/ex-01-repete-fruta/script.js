a7f2a4ed0f0a1e22be3b17742861e4113ec94dc4
Uryel Farias <uryel.farias@gmail.com>
2025-07-15 00:22:54 -0300

    Exercicio 1 do capitulo de repetições, finalizado

--- Livro-LP-e-Algoritmos/04-Lacos/ex-01-repete-fruta/script.js
+++ Livro-LP-e-Algoritmos/04-Lacos/ex-01-repete-fruta/script.js
@@ -5,10 +5,19 @@ botao.addEventListener("click", () => {
     const fruta = document.querySelector('#fruta').value;
     const quantidade = Number(document.querySelector('#numero').value);
     let resultado = document.querySelector('#resultado');
+    let textoResultado = '';
+
+    for(let i = 1; i <= quantidade; i++){
+        if (i === quantidade) {
+            textoResultado = textoResultado + `${fruta}`;
+        } else {
+            textoResultado = textoResultado + `${fruta} * `;
+        }
+    }
 
     console.log(fruta);
     console.log(quantidade);
 
-    resultado.innerText = `${fruta}`;
+    resultado.innerText = `${textoResultado}`;
 })
 
