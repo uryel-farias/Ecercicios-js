b9dbd27ad23e56dbcff4d78e93e789a675198bc3
Uryel Farias <uryel.farias@gmail.com>
2025-07-17 23:54:01 -0300

    Pegando os elementos do exercicio 2, cap 4

--- Livro-LP-e-Algoritmos/04-Lacos/ex-02-chinchilas/script.js
+++ Livro-LP-e-Algoritmos/04-Lacos/ex-02-chinchilas/script.js
@@ -3,5 +3,17 @@ const botao  = document.querySelector('#botao');
 
 botao.addEventListener("click", () => {
 
+    const chincilas = Number(document.querySelector("#chinchila").value);
+    const anos = Number(document.querySelector("#ano").value);
+    let resposta = document.querySelector('#resultado').value;
+    let contador = 1
+    do {
+        
+        contador += 1;
+    } while (anos <= contador)
+
+    console.log(chincilas);
+    console.log(anos);
+
 })
 
