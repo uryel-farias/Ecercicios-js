3928cc5bd38a99318c552a8c69888026b70d5468
Uryel Farias <uryel.farias@gmail.com>
2025-07-18 11:20:41 -0300

    testando solução criando elementos, exercicio 2, cap 4

--- Livro-LP-e-Algoritmos/04-Lacos/ex-02-chinchilas/script.js
+++ Livro-LP-e-Algoritmos/04-Lacos/ex-02-chinchilas/script.js
@@ -1,3 +1,5 @@
+const { createElement } = require("react");
+
 const botao  = document.querySelector('#botao');
 
 
@@ -5,13 +7,18 @@ botao.addEventListener("click", () => {
 
     const chincilas = Number(document.querySelector("#chinchila").value);
     const anos = Number(document.querySelector("#ano").value);
-    let resposta = document.querySelector('#resultado').value;
-    let contador = 1
-    do {
-        
-        contador += 1;
-    } while (anos <= contador)
+    let resposta = document.querySelector(".resultados");
+    let contador = 1;
 
+    do {
+        const elemento = document.createElement("p");
+        elemento.setAttribute
+        contador += 1;
+
+    } while (contador <= anos);
+
+
+    //resposta.innerHTML = `exemplo${anos}`
     console.log(chincilas);
     console.log(anos);
 
