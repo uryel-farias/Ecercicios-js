66a1e220016e933e53ae32efb7f5eca2a30d60ba
Uryel Farias <uryel.farias@gmail.com>
2025-07-21 22:47:31 -0300

    recomeçando a solução do exercicio

--- Livro-LP-e-Algoritmos/04-Lacos/ex-02-chinchilas/script.js
+++ Livro-LP-e-Algoritmos/04-Lacos/ex-02-chinchilas/script.js
@@ -1,5 +1,3 @@
-const { createElement } = require("react");
-
 const botao  = document.querySelector('#botao');
 
 
@@ -8,18 +6,7 @@ botao.addEventListener("click", () => {
     const chincilas = Number(document.querySelector("#chinchila").value);
     const anos = Number(document.querySelector("#ano").value);
     let resposta = document.querySelector(".resultados");
-    let contador = 1;
 
-    do {
-        const elemento = document.createElement("p");
-        elemento.setAttribute
-        contador += 1;
-
-    } while (contador <= anos);
-
-
-    //resposta.innerHTML = `exemplo${anos}`
-    console.log(chincilas);
     console.log(anos);
 
 })
