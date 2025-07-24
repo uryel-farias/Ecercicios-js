f47b0b104c15ca908b1d8eda539981a85ff04328
Uryel Farias <uryel.farias@gmail.com>
2025-07-23 22:16:02 -0300

    Logica para o acumulador de divisiveis finalizada, exercicio 3, cap 4

--- Livro-LP-e-Algoritmos/04-Lacos/ex-03-numero-perfeito/script.js
+++ Livro-LP-e-Algoritmos/04-Lacos/ex-03-numero-perfeito/script.js
@@ -2,6 +2,29 @@ const botao  = document.querySelector('#botao');
 
 
 botao.addEventListener("click", () => {
+    const numero = Number(document.querySelector('#numero').value);
+    let acumulador = 0;
+    let resto;
+
+    for (let index = 1; index < numero; index++) {
+        console.log(`Index = ${index}`)
+        resto = numero % index;
+        console.log(`resto = ${resto}`)
+
+        if(resto === 0) {
+            acumulador += index;
+        }
+
+        console.log(`---------------------------`)
+        
+    }
+    
+    console.log(`*******************************`)
+    console.log(`Acumulador = ${acumulador}`)
+    console.log(`------------FIM----------------`)
+    //console.log(`Acumuluador = ${acumulador}`)
+    //console.log(`Número = ${numero}`)
+
 
 
 
